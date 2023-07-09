c29cbc130773ab342c1e315675a005648e075929
MyManny <gmail.com>
2023-07-09 11:36:48 -0700

    update1

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function createNewImage(src, alt, width, height, left, bottom) {
+    var image = document.createElement('img');
+    
+    image.src = src;
+    
+    image.alt = alt;
+    image.width = width;
+    image.height = height;
+    image.style.position = 'fixed';
+    image.style.left = left + 'px';
+    image.style.bottom = bottom + 'px';
+    
+    document.body.appendChild(image);
+  }
+  
+  createNewImage('assets/green-character.gif', 'Green Character', 200, 200, 100, 100);
+  
+  createNewImage('assets/pine-tree.png', 'Pine Tree', 200, 200, 450, 200);
+  
+  createNewImage('assets/tree.png', 'Tree', 200, 200, 200, 300);
+  
+  createNewImage('assets/pillar.png', 'Pillar', 200, 200, 350, 100);
+  
+  createNewImage('assets/crate.png', 'Crate', 200, 200, 150, 200);
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+  createNewImage('assets/well.png', 'Well', 200, 200, 500, 425);
