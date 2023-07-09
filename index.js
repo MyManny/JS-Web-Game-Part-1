3d2b5fded2eaea1d8e78e12981c764fd185e2ca3
MyManny <gmail.com>
2023-07-09 12:22:53 -0700

    moved sword

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-function createNewImage(src, alt, width, height, left, bottom) {
+function createNewImage(src, alt, left, bottom) {
     var image = document.createElement('img');
-    
     image.src = src;
-    
     image.alt = alt;
-    image.width = width;
-    image.height = height;
     image.style.position = 'fixed';
     image.style.left = left + 'px';
     image.style.bottom = bottom + 'px';
-    
     document.body.appendChild(image);
   }
   
-  createNewImage('assets/green-character.gif', 'Green Character', 200, 200, 100, 100);
+  createNewImage('assets/green-character.gif', 'Green Character', 100, 100);
+  createNewImage('assets/pine-tree.png', 'Pine Tree', 450, 200);
+  createNewImage('assets/tree.png', 'Tree', 200, 300);
+  createNewImage('assets/pillar.png', 'Pillar', 350, 100);
+  createNewImage('assets/crate.png', 'Crate', 150, 200);
+  createNewImage('assets/well.png', 'Well', 500, 425);
   
-  createNewImage('assets/pine-tree.png', 'Pine Tree', 200, 200, 450, 200);
+  function newItem(src, alt, left, bottom) {
+    var item = document.createElement('img');
+    item.src = src;
+    item.alt = alt;
+    item.style.position = 'fixed';
+    item.style.left = left + 'px';
+    item.style.bottom = bottom + 'px';
+    document.body.appendChild(item);
+  }
   
-  createNewImage('assets/tree.png', 'Tree', 200, 200, 200, 300);
+  newItem('assets/sword.png', 'Sword', 500, 405);
   
-  createNewImage('assets/pillar.png', 'Pillar', 200, 200, 350, 100);
-  
-  createNewImage('assets/crate.png', 'Crate', 200, 200, 150, 200);
-
-  createNewImage('assets/well.png', 'Well', 200, 200, 500, 425);
+  var sword = document.querySelector('img[alt="Sword"]');
+  sword.addEventListener('click', function(){
+      sword.remove();
+  });
